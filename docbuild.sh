dadc6782b148f24f7d269061c1646e832928e7a5
straxFromIbr <haga.yuya2.2016@gmail.com>
2021-12-01 10:28:36 +0900

    PDFに変換

--- docbuild.sh
+++ docbuild.sh
@@ -2,13 +2,17 @@
 set -euxo pipefail
 cd "$(dirname "$0")"
 
-
 MDBIN="./docs"
 IMGBIN="./images"
 
 REPORT_MD="report.md"
 REPORT_HTML="report.html"
 REPORT_PDF="report.pdf"
+
+if test "${1:-"build"}" = 'clear'; then
+    rm ${REPORT_MD} ${REPORT_HTML} ${REPORT_PDF}
+    exit
+fi
 
 # * markdownファイルを結合
 # * 画像パスを変更
@@ -19,6 +23,7 @@ ls "${MDBIN}"/*.md \
 
 # * HTMLに変換
 md2html "${REPORT_MD}" > "${REPORT_HTML}"
+
 # * Headless Chromeで変換
 /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome \
     --headless \
